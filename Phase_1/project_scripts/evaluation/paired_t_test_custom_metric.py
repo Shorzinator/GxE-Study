7ead17c2117f3b67851aae3d0ccbe3a87cae24a5
Shorzinator <shorz2905@gmail.com>
2023-08-18 23:26:58 -0500

    performing paired t-test

--- Phase_1/project_scripts/evaluation/paired_t_test_custom_metric.py
+++ Phase_1/project_scripts/evaluation/paired_t_test_custom_metric.py
@@ -2,27 +2,44 @@ import pandas as pd
 from scipy.stats import ttest_rel
 from Phase_1.project_scripts.utility.path_utils import get_path_from_root
 
-# Load the datasets for 1 vs 4 configuration
-path_1 = get_path_from_root("results", "one_vs_all", "logistic_regression_results", "metrics",
-                            "without Race", "with SUT", "AST_3_vs_4_with_SUT.csv")
-path_2 = get_path_from_root("results", "one_vs_all", "logistic_regression_results", "metrics",
-                            "without Race", "AST_3_vs_4_without_SUT.csv")
 
-ast_1_vs_4_with_sut = pd.read_csv(path_1)
-ast_1_vs_4_no_sut = pd.read_csv(path_2)
+categories = [1, 2, 3]
+metrics = ["Accuracy", "Custom_Metric"]
+results = []
 
-# Filter out rows with interaction terms involving SUT
-ast_1_vs_4_with_sut_filtered = ast_1_vs_4_with_sut[~ast_1_vs_4_with_sut['interaction'].str.contains("SubstanceUseTrajectory")]
+for metric in metrics:
+    for category in categories:
 
-# Ensure the orders match - this might not be necessary but is a precaution
-ast_1_vs_4_with_sut_filtered = ast_1_vs_4_with_sut_filtered.sort_values(by='interaction')
-ast_1_vs_4_no_sut = ast_1_vs_4_no_sut.sort_values(by='interaction')
+        # Load the datasets for 1 vs 4 configuration
+        path_1 = get_path_from_root("results", "one_vs_all", "logistic_regression_results", "metrics",
+                                    "without Race", "with SUT", f"AST_{category}_vs_4_with_SUT.csv")
+        path_2 = get_path_from_root("results", "one_vs_all", "logistic_regression_results", "metrics",
+                                    "without Race", f"AST_{category}_vs_4_without_SUT.csv")
 
-# Extract Accuracy values for the 1 vs 4 configuration
-accuracy_with_sut = ast_1_vs_4_with_sut_filtered['Custom_Metric'].values
-accuracy_without_sut = ast_1_vs_4_no_sut['Custom_Metric'].values
+        ast_1_vs_4_with_sut = pd.read_csv(path_1)
+        ast_1_vs_4_no_sut = pd.read_csv(path_2)
 
-# Perform paired t-test
-t_stat, p_value = ttest_rel(accuracy_with_sut, accuracy_without_sut)
+        # Filter out rows with interaction terms involving SUT
+        ast_1_vs_4_with_sut_filtered = ast_1_vs_4_with_sut[~ast_1_vs_4_with_sut['interaction'].str.contains("SubstanceUseTrajectory")]
 
-print(f"t_stat: {t_stat}\n", f"p_value: {p_value}")
+        # Ensure the orders match - this might not be necessary but is a precaution
+        ast_1_vs_4_with_sut_filtered = ast_1_vs_4_with_sut_filtered.sort_values(by='interaction')
+        ast_1_vs_4_no_sut = ast_1_vs_4_no_sut.sort_values(by='interaction')
+
+        # Extract Accuracy values for the 1 vs 4 configuration
+        accuracy_with_sut = ast_1_vs_4_with_sut_filtered[metric].values
+        accuracy_without_sut = ast_1_vs_4_no_sut[metric].values
+
+        # Perform paired t-test
+        t_stat, p_value = ttest_rel(accuracy_with_sut, accuracy_without_sut)
+
+        results.append({
+            "Metric": metric,
+            "Configuration": f"{category}_vs_4",
+            "t_stat": t_stat,
+            "p_value": p_value
+
+        })
+
+results_df = pd.DataFrame(results)
+results_df.to_csv(get_path_from_root("results", "evaluation", "paired_ttest_results.csv"))

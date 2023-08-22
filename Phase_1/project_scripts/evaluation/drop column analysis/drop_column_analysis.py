b695daf9fce710772f132d9a9da87c2f3abfa306
Shorzinator <shorz2905@gmail.com>
2023-08-22 10:00:27 -0500

    Saving script for drop columns analysis

--- Phase_1/project_scripts/evaluation/drop column analysis/drop_column_analysis.py
+++ Phase_1/project_scripts/evaluation/drop column analysis/drop_column_analysis.py
@@ -6,6 +6,10 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.linear_model import LogisticRegression
 
+from Phase_1.config import FEATURES
+from Phase_1.project_scripts.preprocessing.preprocessing import imputation_applier, imputation_pipeline, preprocess_ovr, \
+    scaling_applier, scaling_pipeline, split_data
+from Phase_1.project_scripts.utility.data_loader import load_data_old
 from Phase_1.project_scripts.utility.model_utils import calculate_metrics, ensure_directory_exists, train_model
 from Phase_1.project_scripts.utility.path_utils import get_path_from_root
 
@@ -61,68 +65,78 @@ def visualize_results(df):
     plt.close()
 
 
-def evaluate_with_drop_column(interaction_term, output_column):
+def evaluate_with_drop_column(output_column):
     """
     Evaluates model performance by iteratively dropping columns.
     """
-    # Store results
-    results = []
-    model = LogisticRegression(max_iter=10000, multi_class='ovr', penalty="elasticnet", solver="saga", l1_ratio=0.5)
+    logging.info("Evaluating model performance by iteratively dropping columns ...")
+    ensure_directory_exists(RESULTS_DIR)
 
-    data_1_vs_4 = pd.read_csv(get_path_from_root("data", "processed", "resampled_data_1_vs_4.csv"))
-    data_2_vs_4 = pd.read_csv(get_path_from_root("data", "processed", "resampled_data_2_vs_4.csv"))
-    data_3_vs_4 = pd.read_csv(get_path_from_root("data", "processed", "resampled_data_3_vs_4.csv"))
+    df = load_data_old()
+    datasets = preprocess_ovr(df, output_column)
 
-    datasets = {
-        "1v4": data_1_vs_4,
-        "2v4": data_2_vs_4,
-        "3v4": data_3_vs_4
-    }
+    features = FEATURES.copy()
+    features.remove("PolygenicScoreEXT")
+    fixed_element = "PolygenicScoreEXT"
 
-    for key, data in datasets.items():
-        # Extracting features and target
-        X = data.drop([interaction_term, output_column], axis=1)  # Dropping the interaction column
-        y = data[output_column]
+    feature_pairs = [(fixed_element, x) for x in features if x != fixed_element]
 
-        # Storing the original model's performance as the baseline
-        original_model = train_model(X, y, model)
-        y_pred = original_model.predict(X)
-        baseline_metrics = calculate_metrics(y, y_pred, MODEL_NAME, key, "train")
+    for config_name, (X, y) in datasets.items():
+        # Store results
+        results = []
 
-        for column in X.columns:  # Iterating over feature columns only
-            try:
-                logger.info(f"Training model without {column} column.\n")
-                X_temp = X.drop(column, axis=1)
+        for feature_pair in feature_pairs:
 
-                # Train and get the model
-                model = train_model(X_temp, y, model)
+            # Splitting and preprocessing the data
+            X_train, X_test, y_train, y_test = split_data(X, y)
 
-                # Get predictions and calculate metrics for the training set
-                y_pred_temp = model.predict(X_temp)
-                temp_metrics = calculate_metrics(y, y_pred_temp, MODEL_NAME, key, "train")
+            impute = imputation_pipeline()
+            X_train_imputed = imputation_applier(impute, X_train)
+            X_test_imputed = imputation_applier(impute, X_test)
 
-                change_in_accuracy = baseline_metrics["Accuracy"] - temp_metrics["Accuracy"]
-                change_in_custom_score = baseline_metrics["Custom_Metric"] - temp_metrics["Custom_Metric"]
+            scaler = scaling_pipeline(X_train_imputed.columns.tolist())
+            X_train_scaled, X_test_scaled = scaling_applier(scaler, X_train_imputed, X_test_imputed)
 
-                results.append({
-                                   "type": key, "column_dropped": column, "change_in_accuracy": change_in_accuracy,
-                                   "change_in_custom_score": change_in_custom_score
-                               })
+            model = LogisticRegression(max_iter=10000, multi_class='ovr', penalty="elasticnet", solver="saga", l1_ratio=0.5)
 
-            except Exception as e:
-                logger.error(f"Error encountered when processing column {column}. Error: {str(e)}")
-                continue
+            # Storing the original model's performance as the baseline
+            original_model = train_model(X_train_scaled, y_train, model)
+            y_pred = original_model.predict(X_test_scaled)
+            baseline_metrics = calculate_metrics(y_test, y_pred, MODEL_NAME, config_name, "test")
 
-        logging.info("Saving results ...\n")
-        results_df = pd.DataFrame(results)
-        results_df.to_csv(os.path.join(RESULTS_DIR, "column_drop_evaluation.csv"), index=False)
+            for column in X_train_scaled.columns:  # Iterating over feature columns only
+                try:
+                    logger.info(f"Training model without {column} column.\n")
+                    X_temp = X_train_scaled.drop(column, axis=1)
 
-        logging.info("Visualizing results ...\n")
-        # Visualize the results
-        visualize_results(results_df)
+                    # Train and get the model
+                    model = train_model(X_temp, y_train, model)
+
+                    # Get predictions and calculate metrics for the test set
+                    y_pred_temp = model.predict(X_test_scaled.drop(column, axis=1))
+                    temp_metrics = calculate_metrics(y_test, y_pred_temp, MODEL_NAME, config_name, "test")
+
+                    change_in_accuracy = baseline_metrics["Accuracy"] - temp_metrics["Accuracy"]
+                    change_in_custom_score = baseline_metrics["Custom_Metric"] - temp_metrics["Custom_Metric"]
+
+                    results.append({
+                        "type": config_name, "column_dropped": column, "change_in_accuracy": change_in_accuracy,
+                        "change_in_custom_score": change_in_custom_score
+                    })
+
+                except Exception as e:
+                    logger.error(f"Error encountered when processing column {column}. Error: {str(e)}")
+                    continue
+
+            logging.info("Saving results ...\n")
+            results_df = pd.DataFrame(results)
+            results_df.to_csv(os.path.join(RESULTS_DIR, f"column_drop_evaluation_{config_name}.csv"), index=False)
+
+            logging.info("Visualizing results ...\n")
+            # Visualize the results
+            visualize_results(results_df)
 
 
 if __name__ == "__main__":
-    interaction_term_column = "PolygenicScoreEXT_x_Is_Male"
     output_column_name = "AntisocialTrajectory"
-    evaluate_with_drop_column(interaction_term_column, output_column_name)
+    evaluate_with_drop_column(output_column_name)

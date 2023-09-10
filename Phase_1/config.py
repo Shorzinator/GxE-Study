bc7a5b9963ed18bf836512c1ebc3d564468f67cb
Shorzinator <shorz2905@gmail.com>
2023-09-09 19:15:58 -0500

    Updating utility files

--- Phase_1/config.py
+++ Phase_1/config.py
@@ -1,14 +1,5 @@
-CROSS_VALIDATOR = "KF"
-GRID_SEARCHER = "GCV"
-BALANCER = "SMOTE"
-IT = "IT"   # Interaction term
 FEATURES = ['PolygenicScoreEXT', 'Age', 'DelinquentPeer', 'SchoolConnect',
             'NeighborConnect', 'ParentalWarmth', 'Is_Male']
 
 FEATURES_FOR_AST = FEATURES + ["SubstanceUseTrajectory"]
 FEATURES_FOR_SUT = FEATURES + ["AntisocialTrajectory"]
-
-TARGET_1 = "AST"
-TARGET_2 = "SUT"
-
-COMBINED = f"{BALANCER}_{GRID_SEARCHER}_{CROSS_VALIDATOR}"

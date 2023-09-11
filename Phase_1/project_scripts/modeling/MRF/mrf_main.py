a97387ca209e89168cf5cb8ca1867c63590f87ba
Shorzinator <shorz2905@gmail.com>
2023-09-10 23:25:35 -0500

    Updating modelling files

--- Phase_1/project_scripts/modeling/MRF/mrf_main.py
+++ Phase_1/project_scripts/modeling/MRF/mrf_main.py
@@ -12,9 +12,9 @@ from Phase_1.config import FEATURES_FOR_AST, FEATURES_FOR_SUT
 from Phase_1.project_scripts import get_path_from_root, load_data_old
 from Phase_1.project_scripts.modeling.MRF.mrf_utils import create_mrf_structure, logistic_regression_pairwise_potential, \
     unary_potential
-from Phase_1.project_scripts.preprocessing.mrf_preprocessing import apply_preprocessing_without_interaction_terms_mrf, \
-    preprocess_for_mrf
-from Phase_1.project_scripts.utility.model_utils import ensure_directory_exists
+from Phase_1.project_scripts.preprocessing.mrf_preprocessing import primary_preprocessing_mrf, \
+    secondary_preprocessing_without_interaction_mrf
+from Phase_1.project_scripts.utility.model_utils import calculate_metrics, ensure_directory_exists, save_results
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 warnings.filterwarnings("ignore", category=UserWarning)
 
 MODEL_NAME = "markov_random_field"
-RESULTS_DIR = get_path_from_root("results", "one_vs_all", f"{MODEL_NAME}_results")
+RESULTS_DIR = get_path_from_root("results", "multi_class", f"{MODEL_NAME}_results")
 
 
 def main(target):
@@ -42,15 +42,25 @@ def main(target):
         features_to_use = FEATURES_FOR_SUT
 
     # Applying primary preprocessing
-    data_preprocessed, features = preprocess_for_mrf(df, features_to_use, target)
+    data_preprocessed, features = primary_preprocessing_mrf(df, features_to_use, target)
 
     X = pd.DataFrame(data_preprocessed[features])   # feature dataset
     y = pd.DataFrame(data_preprocessed[target])     # outcome dataset
 
     # Applying secondary preprocessing
-    X_train, y_train, X_val, y_val, X_test, y_test = apply_preprocessing_without_interaction_terms_mrf(X, y, features)
+    X_train, y_train, X_val, y_val, X_test, y_test = secondary_preprocessing_without_interaction_mrf(X, y, features)
+
+    logger.info("Checking for null values...\n")
+    if X_train.isnull().any().any() or y_train.isnull().any().any():
+        print("NaN values detected in the training set after secondary preprocessing!")
+    if X_val.isnull().any().any() or y_val.isnull().any().any():
+        print("NaN values detected in the validation set after secondary preprocessing!")
+    if X_test.isnull().any().any() or y_test.isnull().any().any():
+        print("NaN values detected in the test set after secondary preprocessing!")
+    logger.info("Check for null values complete...\n")
 
     # Define the MRF graphical model
+    logger.info("Defining the MRF graphical model...\n")
     mrf_graph_list = create_mrf_structure(data_preprocessed.columns)
     mrf_graph = nx.Graph(mrf_graph_list)
     model = MarkovNetwork()
@@ -58,6 +68,7 @@ def main(target):
     model.add_edges_from(mrf_graph.edges())
 
     # Set up potential functions
+    logger.info("Setting up unary potential functions...\n")
     for node in mrf_graph.nodes():
         factor = DiscreteFactor([node], [2],
                                 [unary_potential(data_preprocessed, node, 0),
@@ -65,6 +76,7 @@ def main(target):
         model.add_factors(factor)
 
     # Pairwise potentials using logistic regression
+    logger.info("Setting up pairwise potential functions using logistic regression...\n")
     for edge in mrf_graph.edges():
         node1, node2 = edge
         potential_function = logistic_regression_pairwise_potential(data_preprocessed, node1, node2)
@@ -77,6 +89,7 @@ def main(target):
         model.add_factors(factor)
 
     # Inference (as an example)
+    logger.info("Performing inference...\n")
     bp = BeliefPropagation(model)
 
     # Evidence variables for the model. These can be adjusted based on the available data or experiment design.
@@ -87,20 +100,33 @@ def main(target):
         'ParentalWarmth': 1
     }
 
+    logger.info("Making predictions...\n")
+    y_pred = []
+    results = []
     if target == "AntisocialTrajectory":
-        prediction_AST = bp.query(variables=["AntisocialTrajectory"], evidence=evidence_variables)
-        print("Predicted Marginals for AST:", prediction_AST)
+        for index, row in X_test.iterrows():
+            evidence = {var: row[var] for var in evidence_variables}
+            prediction = bp.map_query(variables=["AntisocialTrajectory"], evidence=evidence)
+            y_pred.append(prediction["AntisocialTrajectory"])
+
+        metrics = calculate_metrics(y_test, y_pred, model_name="MRF", target="AntisocialTrajectory",
+                                    test_or_train="Test")
+        results.append({"test_metrics": metrics})
 
     if target == "SubstanceUseTrajectory":
-        prediction_SUT = bp.query(variables=["SubstanceUseTrajectory"], evidence=evidence_variables)
-        print("Predicted Marginals for SUT:", prediction_SUT)
+        for index, row in X_test.iterrows():
+            evidence = {var: row[var] for var in evidence_variables}
+            prediction = bp.map_query(variables=["SubstanceUseTrajectory"], evidence=evidence)
+            y_pred.append(prediction["SubstanceUseTrajectory"])
 
-    # Save results
-    # Placeholder: results should be the metrics or any other relevant data you want to save
-    # results = {}
-    # save_results(target, "MRF", results, metrics_dir, MODEL_NAME)
+        metrics = calculate_metrics(y_test, y_pred, model_name="MRF", target="SubstanceUseTrajectory",
+                                    test_or_train="Test")
+        results.append({"test_metrics": metrics})
 
-    logger.info(f"Markov Random Field modeling for {target} completed.\n")
+    # Save results using the provided save_results function
+    save_results(target, "binary", results, metrics_dir, interaction=False, model_name=MODEL_NAME)
+
+    logger.info(f"Markov Random Field modeling for {target} completed.\\n")
 
 
 if __name__ == '__main__':
--- Phase_1/project_scripts/modeling/MRF/mrf_utils.py
+++ Phase_1/project_scripts/modeling/MRF/mrf_utils.py
@@ -1,79 +1,27 @@
-import numpy as np
-from sklearn.linear_model import LogisticRegression
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def check_nan_values(data, context_msg=""):
+    """Utility function to check and log NaN values."""
+
+    nan_columns = data.columns[data.isnull().any()].tolist()
+    if nan_columns:
+        nan_percentage = data[nan_columns].isnull().mean() * 100
+
+        for column, percentage in nan_percentage.items():
+            logger.warning(f"NaN percentage in column '{column}' ({context_msg}): {percentage:.2f}%")
+        return True
+    return False
 
 
 def create_mrf_structure(variables):
     """
-    Create a fully connected graph structure for the MRF.
+    Create a fully connected graph structure for teh MRF
 
-    Args:
-    - variables: List of variables for the MRF.
-
-    Returns:
-    - A list of edges representing the MRF structure.
-    """
-    edges = []
-    for i in range(len(variables)):
-        for j in range(i+1, len(variables)):
-            if variables[i] != variables[j]:
-                edges.append((variables[i], variables[j]))
-
-    edges = list(set(edges))
-    return edges
-
-
-def unary_potential(data, variable, value):
-    """
-    Calculate the unary potential for a given variable and value based on its distribution in the dataset.
-
-    Args:
-    - data: The dataset.
-    - variable: The variable of interest.
-    - value: The value of the variable.
-
-    Returns:
-    - Unary potential for the given variable and value.
-    """
-    p = np.mean(data[variable] == value)
-    return p
-
-
-def logistic_regression_pairwise_potential(data, variable1, variable2, covariates=[]):
-    """
-    Calculate the pairwise potential using logistic regression.
-
-    Args:
-    - data: The dataset.
-    - variable1: The dependent variable.
-    - variable2: The independent variable.
-    - covariates: List of covariate variables to be included in the regression.
-
-    Returns:
-    - A function that gives pairwise potential based on values of variable1 and variable2.
+    :param variables: Input values for nodes.
+    :return: Edges
     """
 
-    # Prepare the data
-    X = data[[variable2] + covariates]
-    y = data[variable1]
-
-    # Train a logistic regression model
-    clf = LogisticRegression(max_iter=10000, multi_class='ovr')
-    clf.fit(X, y)
-
-    def potential(value1, value2, covariate_values=[]):
-        # Prepare the input data including covariate values
-        input_data = [value2] + covariate_values
-        prob = clf.predict_proba([input_data])[0]
-
-        # Find the index of the value1 in the classes_ attribute of the classifier
-        value1_index = list(clf.classes_).index(value1)
-
-        # Return the probability of variable1 taking value1 given variable2=value2 and covariates
-        return prob[value1_index]
-
-    return potential
-
-# Older pairwise potential function (retained for reference)
-# def pairwise_potential(data, variable1, variable2, value1, value2):
-#    joint_probability = np.mean((data[variable1] == value1) & (data[variable2] == value2))
-#    return joint_probability

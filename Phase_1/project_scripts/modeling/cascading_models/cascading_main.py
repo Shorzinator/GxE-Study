ba2c189a157cec462c6882d716f5e53b8091dd2b
Shorzinator <shorz2905@gmail.com>
2023-09-25 20:37:45 -0500

    Creating a more modular cascading approach

--- Phase_1/project_scripts/modeling/cascading_models/cascading_main.py
+++ Phase_1/project_scripts/modeling/cascading_models/cascading_main.py
@@ -1,11 +1,10 @@
-import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, precision_score, r2_score, roc_auc_score
 
+from Phase_1.project_scripts.modeling.cascading_models.cascading_utils import *
+from Phase_1.project_scripts.preprocessing.preprocessing import *
 from Phase_1.project_scripts.utility.data_loader import load_data_old
 from Phase_1.project_scripts.utility.model_utils import *
-from Phase_1.project_scripts.utility.path_utils import get_path_from_root
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -18,74 +17,6 @@ METRICS_DIR = os.path.join(RESULTS_DIR, "metrics")
 GRAPH_DIR = os.path.join(RESULTS_DIR, "graphs")
 ensure_directory_exists(METRICS_DIR)
 ensure_directory_exists(GRAPH_DIR)
-
-
-def save_performance_metrics_csv(metrics, target):
-    """
-    Save the performance metrics to a CSV file
-
-    :param metrics: Dict, performance metrics
-    :param target: str, target variable name
-    """
-    filename = f"{target}_performance.csv"
-    filepath = os.path.join(METRICS_DIR, filename)
-
-    # Convert dictionary to DataFrame for easier CSV saving
-    metrics_df = pd.DataFrame([metrics])
-
-    # Save to CSV
-    metrics_df.to_csv(filepath, index=False)
-
-    logger.info(f"Performance metrics saved to {filepath}...\n")
-
-
-def evaluate_model(predictions, y_true):
-    """
-    Evaluate model performance on given predictions and true labels.
-
-    :param predictions: array-like, model predictions
-    :param y_true: array-like, true labels
-    :return: dict, containing evaluation metrics
-    """
-    accuracy = accuracy_score(y_true, predictions)
-    precision = precision_score(y_true, predictions, average="micro")
-    auc = roc_auc_score(y_true, predictions)
-
-    metrics = {
-        "accuracy": accuracy,
-        "precision": precision,
-        "auc": auc
-    }
-
-    logger.info(f"Accuracy: {accuracy:.4f}")
-    logger.info(f"Precision: {precision:.4f}")
-    logger.info(f"AUC-ROC: {auc:.4f}")
-
-    return metrics
-
-
-def evaluate_regression_model(predictions, y_true):
-    """
-    Evaluate regression model performance on given predictions and true labels.
-
-    :param predictions: Array-like, model predictions
-    :param y_true: Array-like, true labels
-    :return: Dict, containing evaluation metrics
-    """
-
-    mse = mean_squared_error(y_true, predictions)
-    rmse = np.sqrt(mse)
-    mae = mean_absolute_error(y_true, predictions)
-    r2 = r2_score(y_true, predictions)
-
-    metrics = {
-        "MSE": mse,
-        "RMSE": rmse,
-        "MAE": mae,
-        "R-squared": r2
-    }
-
-    return metrics
 
 
 def main(target):
@@ -146,22 +77,13 @@ def main(target):
         logger.info("Model 2 complete...\n")
 
     # For Model 3: G -> E
-    logger.info("Model 3 started...\n")
+
+    # Starting model 3(a)
+    logger.info("Model 3(a) started...\n")
 
     E_outcomes = ['Age', 'DelinquentPeer', 'SchoolConnect', 'NeighborConnect', 'ParentalWarmth', 'Is_Male']
     X = pd.DataFrame(df[["PolygenicScoreEXT"]])
     y = pd.DataFrame(df[E_outcomes])
-
-    """
-    Na values - 
-    Age                  2
-    DelinquentPeer      42
-    SchoolConnect       39
-    NeighborConnect     28
-    ParentalWarmth     214
-    Is_Male              0
-    dtype: int64
-    """
 
     # Primary preprocessing
     y = y.apply(lambda col: col.fillna(col.mean()))
@@ -170,19 +92,6 @@ def main(target):
     # X_train, y_train, X_val, y_val, X_test, y_test = ap_without_it_genetic(X, y, ["PolygenicScoreEXT"])
     X_train, y_train, X_test, y_test = ap_without_it_genetic(X, y, ["PolygenicScoreEXT"])
 
-    """
-    # Initialize KNNImputer
-    imputer = KNNImputer(n_neighbors=5)
-
-    # Fit on y_train
-    imputer.fit(y_train)
-
-    # Transform (impute) the datasets
-    y_train_imputed = imputer.transform(y_train)
-    # y_val_imputed = imputer.transform(y_val)
-    y_test_imputed = imputer.transform(y_test)
-    """
-
     # Defining the model
     model_3 = RandomForestRegressor(random_state=42)
 
@@ -190,18 +99,57 @@ def main(target):
     param_grid = None
 
     # Training the model
-    best_model_3 = train_model(X_train, y_train, model_3, param_grid)
+    best_model_3a = train_model(X_train, y_train, model_3, param_grid)
 
     # Making predictions based on the best model received.
-    predictions_3 = best_model_3.predict(X_test)
+    predictions_3 = best_model_3a.predict(X_test)
 
     # Calculating metrics to judge model performance.
-    metrics_3 = evaluate_regression_model(predictions_3, y_test)
+    metrics_3a = evaluate_regression_model(predictions_3, y_test)
 
     # Saving model performance for further analysis.
-    save_performance_metrics_csv(metrics_3, f"{target}_Model3")
+    save_performance_metrics_csv(metrics_3a, f"{target}_Model3(a)")
 
-    logger.info("Model 3 complete...\n")
+    compute_and_plot_shap_values(best_model_3a, X_train, X_test, features=["PolygenicScoreEXT"],
+                                 outcome_names=E_outcomes)
+
+    logger.info("Model 3(a) complete...\n")
+
+    # Starting Model 3(b)
+    logger.info("Model 3(b) started...\n")
+
+    for outcome in E_outcomes:
+        logger.info(f"Predicting for outcome: {outcome}...\n")
+
+        X = pd.DataFrame(df[["PolygenicScoreEXT"]])
+        y = pd.DataFrame(df[outcome])
+
+        # Primary preprocessing
+        y = y.fillna(y.mean())
+
+        # Secondary Preprocessing
+        X_train, y_train, X_test, y_test = ap_without_it_genetic(X, y, ["PolygenicScoreEXT"])
+
+        # Defining the model
+        model_3b = RandomForestRegressor(random_state=42)
+
+        # Defining parameter grid for hyperparameter tuning; None implies no tuning being done.
+        param_grid = None
+
+        # Training the model
+        y_train = y_train.values.ravel()
+        best_model_3b = train_model(X_train, y_train, model_3b, param_grid)
+
+        # Making predictions based on the best model received.
+        predictions_3b = best_model_3b.predict(X_test)
+
+        # Calculating metrics to judge model performance.
+        metrics_3b = evaluate_regression_model(predictions_3b, y_test)
+
+        # Saving model performance for further analysis.
+        save_performance_metrics_csv(metrics_3b, f"{target}_{outcome}_Model3(b)")
+
+    logger.info("Model 3(b) complete...\n")
 
     logger.info(f"Cascading approach for {target} completed...")
 
---
+++ Phase_1/project_scripts/modeling/cascading_models/cascading_utils.py
@@ -0,0 +1,133 @@
+import logging
+import os
+import traceback
+
+import numpy as np
+import pandas as pd
+import shap
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score, mean_absolute_error, mean_squared_error, precision_score, r2_score, \
+    roc_auc_score
+
+from Phase_1.project_scripts.utility.model_utils import ensure_directory_exists
+from Phase_1.project_scripts.utility.path_utils import get_path_from_root
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+MODEL_NAME = "cascading"
+RESULTS_DIR = get_path_from_root("results", "one_vs_all", f"{MODEL_NAME}_results")
+
+# Subdirectories for metrics
+METRICS_DIR = os.path.join(RESULTS_DIR, "metrics")
+GRAPH_DIR = os.path.join(RESULTS_DIR, "graphs")
+ensure_directory_exists(METRICS_DIR)
+ensure_directory_exists(GRAPH_DIR)
+
+
+def compute_and_plot_shap_values(model, X_train, X_test, features=None, outcome_names=None):
+    """
+    Compute and plot SHAP values for a given model and dataset.
+
+    :param model:
+    :param X_train:
+    :param X_test:
+    :param features:
+    :return:
+    """
+
+    logging.info("Starting SHAP analysis...\n")
+    try:
+        # Initialize the explainer
+        logging.info("Initializing the explainer...\n")
+        explainer = shap.KernelExplainer(model.predict, X_train.sample(100))
+
+        # Compute SHAP values for the test set
+        logging.info("Compute SHAP values for the test set...\n")
+        shap_values = explainer.shap_values(X_test)
+
+        # Consolidate SHAP values by averaging across outcomes
+        consolidated_shap_values = np.mean(shap_values, axis=0)
+
+        os.makedirs(GRAPH_DIR, exist_ok=True)
+
+        # Plot consolidated SHAP values
+        shap.summary_plot(consolidated_shap_values, X_train, feature_names=features, plot_type="bar", show=False)
+        plt.title("Consolidated SHAP values across all outcomes")
+
+        # Save the plot
+        filename = os.path.join(GRAPH_DIR, f"consolidated_shap_values.png")
+        plt.savefig(filename, bbox_inches='tight')
+        plt.close()
+
+    except Exception as e:
+        print(f"Error computing or plotting SHAP values: {e}")
+        print(traceback.format_exc())
+
+
+def save_performance_metrics_csv(metrics, target):
+    """
+    Save the performance metrics to a CSV file
+
+    :param metrics: Dict, performance metrics
+    :param target: str, target variable name
+    """
+    filename = f"{target}_performance.csv"
+    filepath = os.path.join(METRICS_DIR, filename)
+
+    # Convert dictionary to DataFrame for easier CSV saving
+    metrics_df = pd.DataFrame([metrics])
+
+    # Save to CSV
+    metrics_df.to_csv(filepath, index=False)
+
+    logger.info(f"Performance metrics saved to {filepath}...\n")
+
+
+def evaluate_model(predictions, y_true):
+    """
+    Evaluate model performance on given predictions and true labels.
+
+    :param predictions: array-like, model predictions
+    :param y_true: array-like, true labels
+    :return: dict, containing evaluation metrics
+    """
+    accuracy = accuracy_score(y_true, predictions)
+    precision = precision_score(y_true, predictions, average="micro")
+    auc = roc_auc_score(y_true, predictions)
+
+    metrics = {
+        "accuracy": accuracy,
+        "precision": precision,
+        "auc": auc
+    }
+
+    logger.info(f"Accuracy: {accuracy:.4f}")
+    logger.info(f"Precision: {precision:.4f}")
+    logger.info(f"AUC-ROC: {auc:.4f}")
+
+    return metrics
+
+
+def evaluate_regression_model(predictions, y_true):
+    """
+    Evaluate regression model performance on given predictions and true labels.
+
+    :param predictions: Array-like, model predictions
+    :param y_true: Array-like, true labels
+    :return: Dict, containing evaluation metrics
+    """
+
+    mse = mean_squared_error(y_true, predictions)
+    rmse = np.sqrt(mse)
+    mae = mean_absolute_error(y_true, predictions)
+    r2 = r2_score(y_true, predictions)
+
+    metrics = {
+        "MSE": mse,
+        "RMSE": rmse,
+        "MAE": mae,
+        "R-squared": r2
+    }
+
+    return metrics

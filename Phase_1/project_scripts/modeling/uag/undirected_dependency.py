48cf255f6e3df818b27527608873a6f203fa4528
Shorzinator <shorz2905@gmail.com>
2023-11-05 12:28:30 -0600

    Adding png of graphs and updating viz script

--- Phase_1/project_scripts/modeling/uag/undirected_dependency.py
+++ Phase_1/project_scripts/modeling/uag/undirected_dependency.py
@@ -1,18 +1,16 @@
+import logging
 import os
 
-import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
 import pandas as pd
 import statsmodels.api as sm
-import networkx as nx
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
 
 from Phase_1.config import FEATURES_FOR_AST, FEATURES_FOR_SUT
 from Phase_1.project_scripts import get_path_from_root, load_data_old
 from Phase_1.project_scripts.preprocessing.mrf_preprocessing import primary_preprocessing_mrf, \
     secondary_preprocessing_without_interaction_mrf
 from Phase_1.project_scripts.utility.model_utils import ensure_directory_exists
-import logging
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -41,7 +39,7 @@ def neighborhood_regression(target):
     data_preprocessed, features = primary_preprocessing_mrf(df, features_to_use, target)
 
     X = pd.DataFrame(data_preprocessed[features])  # feature dataset
-    y = pd.DataFrame(data_preprocessed[target])    # outcome dataset
+    y = pd.DataFrame(data_preprocessed[target])  # outcome dataset
 
     # Applying secondary preprocessing
     X_train, y_train, X_test, y_test = secondary_preprocessing_without_interaction_mrf(X, y, features)
@@ -54,6 +52,14 @@ def neighborhood_regression(target):
     y_combined.reset_index(drop=True, inplace=True)
 
     data_combined = pd.concat([X_combined, y_combined], axis=1)
+
+    # Neighborhood Regression
+    graph_edges = []
+    edge_weights = {}
+    edge_labels = {}
+
+    # Define edge colors base on significance
+    edge_colors = []
 
     # Neighborhood Regression
     graph_edges = []
@@ -70,65 +76,51 @@ def neighborhood_regression(target):
             if (target_var, predictor) not in graph_edges and (predictor, target_var) not in graph_edges:
                 graph_edges.append((target_var, predictor))
 
-    # Define your genetic and environmental features explicitly
-    genetic_feature = 'PolygenicScoreEXT'
-    environmental_features = ['Age', 'Sex', 'PolygenicScoreEXT_x_Age', 'DelinquentPeers', 'SchoolConnect',
-                              'NeighborConnect', 'ParentalWarmth', 'PolygenicScoreEXT_x_Is_Male',
-                              'SubstanceUseTrajectory']
-    outcome_variable = target
-
-    # Graph Construction with updated nodes based on the actual feature names
+    # Graph Construction
     graph = nx.DiGraph()
     graph.add_edges_from(graph_edges)
 
-    # Define the shell positions for the graph layout
-    shell_pos = [[genetic_feature], environmental_features, [outcome_variable]]
-    pos = nx.shell_layout(graph, shell_pos)
+    # Getting regression coefficients and p-values for edges
+    edge_weights = []
+    edge_colors = []
+    edge_labels = {}
+    for edge in graph_edges:
+        predictors = data_combined.columns.drop(edge[0]).tolist()
+        X = sm.add_constant(data_combined[predictors])
+        model = sm.OLS(data_combined[edge[0]], X).fit()
+        coef = model.params[edge[1]]
+        p_value = model.pvalues[edge[1]]
 
-    # Node Customization - Now you need to base this on the actual feature names rather than the prefixes
-    node_colors = []
-    node_shapes = []
-    for node in graph.nodes():
-        if node == genetic_feature:
-            node_colors.append("#1f77b4")  # Blue for Genetic Feature
-            node_shapes.append('s')  # Square for Genetic Feature
-        elif node in environmental_features:
-            node_colors.append("#ff7f0e")  # Orange for Environmental Features
-            node_shapes.append('o')  # Circle for Environmental Features
-        elif node == outcome_variable:
-            node_colors.append("#2ca02c")  # Green for Outcome Variable
-            node_shapes.append('d')  # Diamond for Outcome Variable
+        edge_weights.append(abs(coef) * 5)  # Scale factor for visualization
+        edge_colors.append(1 - min(p_value, 0.1))  # Makes edges with p-value > 0.1 almost transparent
+
+        # Only annotate edges with p-value < 0.05 for clarity
+        if p_value < 0.05:
+            edge_labels[edge] = f"{coef:.2f}"
+
+    # Create DataFrame for edge data
+    edges_df = pd.DataFrame(graph_edges, columns=['From', 'To'])
+    edges_df['Weight'] = [edge_labels.get(edge, 'n/s') for edge in graph_edges]
+    edges_df['P-value'] = [model.pvalues[edge[1]] for edge in graph_edges]
+
+    # Save to CSV
+    edges_df.to_csv(os.path.join(metrics_dir, 'edges_data.csv'), index=False)
+
+    # Adjusting the Layout
+    pos = nx.kamada_kawai_layout(graph)
 
     # Visualization
+    node_colors = ["#1f77b4" if node.startswith('G') else "#ff7f0e" if node.startswith('E') else "#2ca02c" for node in
+                   graph.nodes()]
+    node_sizes = [500 + 2000 * graph.degree(node) for node in graph.nodes()]  # Adjust size based on degree
+
     plt.figure(figsize=(12, 12))
-
-    # Drawing nodes with a larger size and based on their actual feature categories
-    for shape in set(node_shapes):
-        nx.draw_networkx_nodes(graph, pos,
-                               nodelist=[node for node in graph.nodes() if graph.nodes[node].get('shape') == shape],
-                               node_color=[node_colors[node_shapes.index(shape)]],
-                               node_size=2500, node_shape=shape)
-
-    # Draw edges with the weights and colors
-    nx.draw_networkx_edges(graph, pos, edgelist=graph_edges,
-                           width=[weight for _, weight in edge_weights],
-                           edge_color=[color for _, color in edge_colors],
-                           arrowstyle='-|>', arrowsize=20)
-
-    # Draw edge labels with the weights
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels, font_size=10)
-
-    # Adding legend for the nodes
-    legend_elements = [Line2D([0], [0], marker='s', color='w', label='Genetic Feature (G)', markersize=10,
-                              markerfacecolor="#1f77b4"),
-                       Line2D([0], [0], marker='o', color='w', label='Environmental Features (E)', markersize=10,
-                              markerfacecolor="#ff7f0e"),
-                       Line2D([0], [0], marker='d', color='w', label='Outcome (O)', markersize=10,
-                              markerfacecolor="#2ca02c")]
-    plt.legend(handles=legend_elements, loc="best")
-
+    nx.draw(graph, pos, with_labels=True, node_size=node_sizes, node_color=node_colors, font_size=15,
+            width=edge_weights, edge_color=edge_colors, edge_cmap=plt.cm.Blues, edge_vmin=0, edge_vmax=1,
+            font_weight="bold", alpha=0.9)
+    nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_labels, font_size=10, alpha=0.7)
     plt.title("Feature Relationships Graph")
-    plt.savefig("refined_neighbourhood_regression.png")
+    plt.savefig("neighbourhood_regression_DiGraph.png")
     plt.show()
 
 

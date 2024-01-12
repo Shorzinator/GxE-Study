18fe183d959fade6cd67d5fbc1c4b15fffa9ae44
Shorzinator <shorz2905@gmail.com>
2024-01-12 13:21:05 +0530

    Adding script to extract directory tree

---
+++ Phase_2/update_directory_tree.py
@@ -0,0 +1,24 @@
+import fnmatch
+import os
+
+
+def save_directory_tree(parent_dirs, ignore_dirs, output_file):
+    with open(output_file, 'w') as f:
+        for parent_dir in parent_dirs:
+            for dirpath, dirnames, filenames in os.walk(parent_dir):
+                # Remove directories in ignore_dirs
+                dirnames[:] = [d for d in dirnames if d not in ignore_dirs]
+                for filename in fnmatch.filter(filenames, '*'):
+                    f.write(os.path.join(dirpath, filename) + '\n')
+
+
+# Specify the parent directories
+parent_dirs = ['project_scripts', 'results']
+
+# Specify the directories to ignore
+ignore_dirs = ['.idea', '__pycache__', '.ipynb_checkpoints']
+
+# Specify the output file
+output_file = 'directory_tree.txt'
+
+save_directory_tree(parent_dirs, ignore_dirs, output_file)
